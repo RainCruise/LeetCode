6ed6a860ff667b2c17d524c58067ac9850412592
RainCruise <raincruisethu@gmail.com>
2017-10-09 16:19:09 +0800

    fix zigzag

---
+++ 6.zigzag-conversion.cpp
@@ -0,0 +1,27 @@
+//将字符串转化成正弦函数的形状，就能理解题意
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows <= 1) {
+            return s;
+        }
+        int len = s.length();
+        string *str = new string[numRows];
+        int row = 0, step = 0;
+        for (int i = 0; i < len; ++i) {
+            str[row].push_back(s[i]);
+            if (row == 0) {
+                step = 1;
+            } else if (row == numRows - 1) {
+                step = -1;
+            }
+            row += step;
+        }
+        s.clear();
+        for (int i = 0; i < numRows; ++i) {
+            s.append(str[i]);
+        }
+        delete[] str;
+        return s;
+    }
+};
