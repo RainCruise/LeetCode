7e244eb98ae07c024da70c29201c578e76d8cdce
RainCruise <raincruisethu@gmail.com>
2017-11-16 23:21:52 +0800

    50-- Math Problems

---
+++ 50.powx-n.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double result = 1;
+        unsigned long long p;
+        if (n < 0) {
+            p = -n;
+            x = 1 / x;
+        } else {
+            p = n;
+        }
+        while (p) {
+            if (p & 1) {
+                result *= x;
+            }
+            x *= x;
+            p >>= 1;
+        }
+        return result;
+    }
+};
